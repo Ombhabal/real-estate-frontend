742d27318715667fa7daf25dccb92a49d9bb9251
Ombhabal <ombhabal@gmail.com>
2024-10-07 18:12:26 +0530

    first commit

---
+++ src/routes/homePage/homePage.jsx
@@ -0,0 +1,42 @@
+import { useContext } from "react";
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+import { AuthContext } from "../../context/AuthContext";
+
+function HomePage() {
+
+  const {currentUser} = useContext(AuthContext)
+
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+          A modern real estate platform offering seamless property browsing, advanced search filters, and user-friendly dashboards for buyers, sellers, and agents—all in one place. Explore detailed listings with high-quality images, pricing, virtual tours, and neighborhood insights. Effortlessly connect with agents, schedule property visits, and manage transactions online!
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
